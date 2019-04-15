7f3bd536bb76fd33a65f62c5907217d0dcd9d043
Patrick El-Hage <patrickelhageuniv@gmail.com>
2019-04-15 23:00:40 +0200

    component refacto

--- pokemon/static/pokemon/vue.js
+++ pokemon/static/pokemon/vue.js
@@ -140,3 +140,116 @@ Vue.component('poke-display', {
     }
   }
 })
+
+Vue.component('poke-stats', {
+  props: ['pokemon'],
+  template: "#poke-stats",
+  methods: {
+    split_cap:function(text) {
+      return split_cap(text);
+    }
+  }
+})
+
+Vue.component('poke-evo', {
+  props: ['pokemon'],
+  template: "#poke-evo",
+  methods: {
+    split_cap:function(text) {
+      return split_cap(text);
+    },
+    evo_ranks:function(evos) {
+      var ranks = new Set();
+      for (i = 0; i < evos.length; i++) {
+        ranks.add(evos[i].evolution_rank);
+      }
+      return ranks;
+    },
+    rank_evos:function(evos, rank) {
+      var ret = [];
+      for (i = 0; i < evos.length; i++) {
+        if (evos[i].evolution_rank == rank) {
+          ret.push(evos[i]);
+        }
+      }
+      return ret;
+    }
+  }
+})
+
+Vue.component('poke-types', {
+  props: ['pokemon'],
+  template: "#poke-types",
+  methods: {
+    split_cap:function(text) {
+      return split_cap(text);
+    }
+  }
+})
+
+Vue.component('poke-moves', {
+  props: ['pokemon', 'version_info'],
+  template: "#poke-moves",
+  methods: {
+    split_cap:function(text) {
+      return split_cap(text);
+    },
+    mid_slash:function(text) {
+      return mid_slash(text);
+    },
+    learn_methods:function(pokemon_moves) {
+      var methods = new Set();
+      for (i = 0; i < pokemon_moves.length; i++){
+        methods.add(pokemon_moves[i].learn_method.name);
+      }
+      return methods;
+    },
+    method_moves:function(pokemon_moves, learn_method) {
+      var moves = [];
+      for (i = 0; i < pokemon_moves.length; i++) {
+        if (pokemon_moves[i].learn_method.name == learn_method) {
+          moves.push(pokemon_moves[i]);
+        }
+      }
+      return moves;
+    },
+    stab_style:function(move, pokemon) {
+      if (move.category == 'Status') {
+        return '';
+      }
+      if (move.type.name == pokemon.primary_type.name) {
+        return ' stab';
+      }
+      if ( (pokemon.secondary_type) && (move.type.name == pokemon.secondary_type.name) ) {
+        return ' stab';
+      }
+      return '';
+    },
+    convert_if_none:function(entry, target) {
+      if (entry) {
+        return entry;
+      }
+      return target;
+    }
+  }
+})
+
+Vue.component('poke-encounters', {
+  props: ['pokemon', 'version_info'],
+  template: "#poke-encounters",
+  methods: {
+    split_cap:function(text) {
+      return split_cap(text);
+    }
+  }
+})
+
+Vue.component('poke-abilities', {
+  props: ['pokemon'],
+  template: "#poke-abilities",
+  methods: {
+    split_cap:function(text) {
+      return split_cap(text);
+    }
+  }
+})
