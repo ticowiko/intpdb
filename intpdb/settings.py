bbf2e8afcda25caf07c67156c46c9a413901c7b0
Patrick El-Hage <patrickelhageuniv@gmail.com>
2019-04-22 19:06:21 +0200

    aesthetic refacto

--- intpdb/settings.py
+++ intpdb/settings.py
@@ -92,25 +92,27 @@ WSGI_APPLICATION = 'intpdb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'intpdb',
-        'USER': 'intpdb_user',
-        'PASSWORD': 'pw',
-        'HOST': 'localhost',
-        'PORT': '',
-    },
-}
-
 if IN_AWS:
-    DATABASES['default'] = {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ['RDS_DB_NAME'],
-        'USER': os.environ['RDS_USERNAME'],
-        'PASSWORD': os.environ['RDS_PASSWORD'],
-        'HOST': os.environ['RDS_HOSTNAME'],
-        'PORT': os.environ['RDS_PORT'],
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'intpdb',
+            'USER': 'intpdb_user',
+            'PASSWORD': 'pw',
+            'HOST': 'localhost',
+            'PORT': '',
+        },
     }
 
 # Caching
